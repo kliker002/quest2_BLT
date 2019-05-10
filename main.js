68bf48146541625202fda4150a718f986c8f9bd8
kliker02 <laker.tv@bk.ru>
2019-05-10 23:20:20 +0300

    fixed wrong input

--- main.js
+++ main.js
@@ -5,55 +5,62 @@ var number = [1, 2, 3, 4, 5, 6, 7, 8];
 var response = [];
 
 function main() {
+
+
     var html_element = document.getElementById('pos');
     var letter = html_element.value[0].toLowerCase();
     var index_letter = letters.indexOf(letter)
+    if (index_letter != -1 && number.indexOf(parseInt(html_element.value[1])) != -1) {
+        vertical_check(html_element.value, index_letter)
+        horizontal_check(html_element.value, index_letter)
+        alert(response)
+        response = []
+    }else{
+        alert('Wrong input!');
+    }
 
-    vertical_check(html_element.value, index_letter)
-    horizontal_check(html_element.value, index_letter)
-    alert(response)
-    response = []
 
 }
-function vertical_check(ask_input, index_letter) { //проверка позиций вертикальных блоков
+function vertical_check(ask_input, index_letter) {
     var result_num = 0;
 
-    if (parseInt(ask_input[1]) + parseInt('2') <= number[number.length - 1]) {//осуществляется проверка на выход за пределы
+    if (parseInt(ask_input[1]) + parseInt('2') <= number[number.length - 1]) { //осуществляется проверка на выход за пределы
 
 
         if (index_letter + parseInt(1) < letters.length) {
-            response.push(letters[index_letter + 1] + (parseInt(ask_input[1]) + parseInt('2')))
+            response.push(letters[index_letter + 1] + (parseInt(ask_input[1]) + parseInt('2')));
         }
         if (index_letter - parseInt(1) >= 0) {
-            response.push(letters[index_letter - 1] + (parseInt(ask_input[1]) + parseInt('2')))
+            response.push(letters[index_letter - 1] + (parseInt(ask_input[1]) + parseInt('2')));
         }
 
     }
-    if (parseInt(ask_input[1]) - parseInt('2') > 0) {//осуществляется проверка на выход за пределы
+    if (parseInt(ask_input[1]) - parseInt('2') > 0) { //осуществляется проверка на выход за пределы
         if (index_letter + parseInt(1) < letters.length) {
-            response.push(letters[index_letter + 1] + (parseInt(ask_input[1]) - parseInt('2')))
+            response.push(letters[index_letter + 1] + (parseInt(ask_input[1]) - parseInt('2')));
         }
         if (index_letter - parseInt(1) >= 0) {
-            response.push(letters[index_letter - 1] + (parseInt(ask_input[1]) - parseInt('2')))
+            response.push(letters[index_letter - 1] + (parseInt(ask_input[1]) - parseInt('2')));
         }
     }
 }
-function horizontal_check(ask_input, index_letter) {//проверка позиций горизонтальных блоков
+function horizontal_check(ask_input, index_letter) {
     numOnDesc = ask_input[1]
-    if (parseInt(index_letter) - parseInt('2') >= 0) {//осуществляется проверка на выход за пределы
-        if (parseInt(numOnDesc) + parseInt('1') < number.length) {
-            response.push(letters[index_letter - 2] + (parseInt(numOnDesc) + 1))
+    if (parseInt(index_letter) - parseInt('2') >= 0) { //осуществляется проверка на выход за пределы
+        if (parseInt(numOnDesc) < number.length) {
+            response.push(letters[index_letter - 2] + (parseInt(numOnDesc) + 1));
         }
         if (parseInt(numOnDesc) - parseInt('1') > 0) {
-            response.push(letters[index_letter - 2] + (parseInt(numOnDesc) - 1))
+            response.push(letters[index_letter - 2] + (parseInt(numOnDesc) - 1));
         }
     }
-    if (parseInt(index_letter) + parseInt('2') < letters.length) {//осуществляется проверка на выход за пределы
-        if (parseInt(numOnDesc) + parseInt('1') < number.length) {
-            response.push(letters[index_letter + 2] + (parseInt(numOnDesc) + 1))
+    if (parseInt(index_letter) + parseInt('2') < letters.length) { //осуществляется проверка на выход за пределы
+        if (parseInt(numOnDesc) < number.length) {
+            response.push(letters[index_letter + 2] + (parseInt(numOnDesc) + 1));
         }
         if (parseInt(numOnDesc) - parseInt('1') > 0) {
-            response.push(letters[index_letter + 2] + (parseInt(numOnDesc) - 1))
+            response.push(letters[index_letter + 2] + (parseInt(numOnDesc) - 1));
+
         }
     }
 }
